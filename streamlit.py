4a0566a7e3ff115f662eb7e807e6b2872a32f44b
estadisticasCame <143653599+estadisticasCame@users.noreply.github.com>
2023-08-31 11:09:56 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -7,34 +7,27 @@ import pandas as pd
 st.set_page_config(
     page_title="Calculadora Ahora 12",
     page_icon="📊",
-    layout="wide",
     )
 
 
-
 # Columnas superiores
-col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3 = st.columns(3)
 
 with col1 :
-    st.image("imgs/CAME-Transparente.png", width=200)
+    st.image("imgs/CAME-Transparente.png")
 
 with col2 : 
-    colA, colB = st.columns([1,2]) 
-    with colA:
-        st.image("imgs/logo_ahora12.png", width=200)
-    with colB:
-        st.title("Calculadora Ahora 12")
-
-    # Realizamos el input del monto
-    monto_credito = st.number_input("Ingrese el monto")
-
-    # Inputo de la cuota
-    cuotas = ["Ahora 3","Ahora 6","Ahora 12","Ahora 18","Ahora 24"]
-    cuota = st.selectbox("Programa",cuotas)
+    st.image("imgs/comercio@4x.png")
     
-
-
-    
-
 with col3 :
-    st.image("imgs/comercio@4x.png", width=200)
+    st.image("imgs/logo_ahora12.png")
+
+# Titulo
+st.title("Calculadora papa")
+
+# Realizamos el input del monto
+monto_credito = st.number_input("Ingrese el monto")
+
+# Inputo de la cuota
+cuotas = ["Ahora 3","Ahora 6","Ahora 12","Ahora 18","Ahora 24"]
+cuota = st.selectbox("Programa",cuotas)    
