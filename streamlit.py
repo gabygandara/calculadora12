71a6ace1acfad4fde4b5d91ee581280234b2d725
estadisticasCame <143653599+estadisticasCame@users.noreply.github.com>
2023-08-31 10:58:32 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -7,29 +7,34 @@ import pandas as pd
 st.set_page_config(
     page_title="Calculadora Ahora 12",
     page_icon="📊",
+    layout="wide",
     )
 
+
+
 # Columnas superiores
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns([1,2,1])
 
 with col1 :
-    st.image("imgs/CAME-Transparente.png" )
+    st.image("imgs/CAME-Transparente.png", use_column_width=True, width=600 , height = 600)
 
 with col2 : 
-    colA, colB = st.columns(2) 
+    colA, colB = st.columns([1,2]) 
     with colA:
-        st.image("imgs/logo_ahora12.png" )
+        st.image("imgs/logo_ahora12.png", use_column_width=True, width=600 , height = 600)
     with colB:
         st.title("Calculadora Ahora 12")
-        # Realizamos el input del monto
-        monto_credito = st.number_input("Ingrese el monto")
-        
-        # Inputo de la cuota
-        cuotas = ["3","6","12","18","24"]
-        cuota = st.selectbox("Cantidad de cuotas",cuotas)
 
+    # Realizamos el input del monto
+    monto_credito = st.number_input("Ingrese el monto")
+
+    # Inputo de la cuota
+    cuotas = ["Ahora 3","Ahora 6","Ahora 12","Ahora 18","Ahora 24"]
+    cuota = st.selectbox("Programa",cuotas)
+    
 
 
     
+
 with col3 :
-    st.image("imgs/comercio@4x.png")
+    st.image("imgs/comercio@4x.png", use_column_width=True, width=600, height = 600 )
