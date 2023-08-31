8b1cc0bfdaedcdeef32d67d2ac312b9ad9e36d41
estadisticasCame <143653599+estadisticasCame@users.noreply.github.com>
2023-08-31 11:38:05 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -10,14 +10,9 @@ st.set_page_config(
     )
 
 # Titulo centrado con columnas
-col11, col22, col33 = st.columns([0.5, 3, 0.5])
 
-with col11:
-    st.write("")
-with col22:
-    st.markdown("#   **Calculadora Ahora 12**")
-with col33:
-    st.write("")
+st.markdown("#   **Calculadora Ahora 12**")
+
 
 # Columnas inferiores
 col1, col2, col3 = st.columns([1,2,1])
