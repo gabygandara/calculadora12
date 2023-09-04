cf19c7280ef5f5f15cc7d2424778a2437e0ee685
estadisticasCame <143653599+estadisticasCame@users.noreply.github.com>
2023-09-04 10:16:40 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -141,14 +141,14 @@ if aux3 == True :
     titulo = "Calculadora Ahora 12"
     titulo_width = c.stringWidth(titulo, "Helvetica-Bold", 32)
     titulo_x = (letter[0] - titulo_width) / 2  # Centrar el título horizontalmente
-    c.drawString(titulo_x, 760, titulo)
+    c.drawString(titulo_x, 740, titulo)
 
     # Coordenadas y dimensiones de la imagen
     imagen_path = "imgs/logos_came_con_fondo y recortados2.png"  # Reemplaza 'tu_imagen.png' con la ruta de tu propia imagen
     imagen_width = 300  # Ancho de la imagen
     imagen_height = 50  # Altura de la imagen
     imagen_x = (letter[0] - imagen_width) / 2  # Centrar la imagen horizontalmente
-    imagen_y = 700  # Espacio entre el título y la imagen
+    imagen_y = 680  # Espacio entre el título y la imagen
 
     c.drawImage(imagen_path, imagen_x, imagen_y, width=imagen_width, height=imagen_height)
 
@@ -156,7 +156,7 @@ if aux3 == True :
     rect_width = 400  # Ancho del rectángulo
     rect_height = 50  # Altura del rectángulo
     rect_x = (letter[0] - rect_width) / 2  # Centrar el rectángulo horizontalmente
-    rect_y = 580 # Espacio entre la imagen y el rectángulo
+    rect_y = 620 # Espacio entre la imagen y el rectángulo
 
     c.rect(rect_x, rect_y, rect_width, rect_height)
 
