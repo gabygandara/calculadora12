36e8255cec920875834e2cd3b15ed80106fd5928
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2024-03-13 13:59:45 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -286,8 +286,8 @@ if (st.session_state.submit_button == True):
             tasa_iibb_str = str(round(tasa_iibb * 100,1)).replace(".",",") 
             
             tasas_cft = {
-                        "3 Cuotas" : 0.0805,
-                        "6 Cuotas" : 0.1505 }
+                        "3 Cuotas" : 0.1076,
+                        "6 Cuotas" : 0.1976 }
             
             # PARA AVANZAR EL CÁLCULO
             if ("Responsable" in st.session_state.tipo_inscripcion) or (st.session_state.tipo_inscripcion == "Sociedad"):
@@ -497,8 +497,8 @@ if (st.session_state.submit_button == True):
             value_x = x2 - 10
             # Espaciado vertical entre las líneas
             line_spacing = 20
-            if tasas_a_STR == "15,049999999999999":
-                    tasas_a_STR = "15,05"    
+            if tasas_a_STR == "19,759999999999998":
+                    tasas_a_STR = "19,76"       
             c.setFont("Helvetica-Bold", 14)
             c.drawString(90, 480, "Liquidación de pago")   
             # Agrega las categorías y valores
@@ -686,8 +686,8 @@ if (st.session_state.submit_button == True):
             if aux == True : 
                 st.write("---")
                 st.write("**Detalle de descuentos:**")
-                if tasas_a_STR == "15,049999999999999":
-                    tasas_a_STR = "15,05"  
+                if tasas_a_STR == "19,759999999999998":
+                    tasas_a_STR = "19,76"    
                 st.write(f"+ Tasa del programa {programa_seleccionado} ({tasas_a_STR}%): **${lista_variables[3]}**")
                 st.write(f"+ Arancel T.Cred (1,8%): **${lista_variables[2]}**")
                 st.write(f"+ IVA (21%): **${lista_variables[4]}**")
